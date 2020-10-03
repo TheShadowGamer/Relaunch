19fd7d56c386f3620e1bedb4ff78ea1727f16fed
Valentine Briese <controlfreak707@gmail.com>
2020-10-03 13:18:01 -0700

    Finally fix that weird spacing issue

--- Settings.jsx
+++ Settings.jsx
@@ -18,6 +18,7 @@ module.exports = class Settings extends React.Component {
           </Text>
         </Card>
         <Divider />
+        <div style={{ marginBottom: 20 }} />
         <ButtonItem
           onClick={() => open(() => React.createElement(ConfirmModal))}
           note="Completely close Discord and relaunch it."
